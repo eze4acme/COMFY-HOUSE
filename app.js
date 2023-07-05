19d3607314c20505a896958739689793aa177499
Eze Onyebuchi <eze4acme@gmail.com>
2023-07-05 14:00:24 +0100

    Update app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+//variables
+
+const cartBtn = document.querySelector('.cart-btn')
+const closeCartBtn = document.querySelector('.close-cart')
+const clearCartBtn = document.querySelector('.clear-cart')
+const cartDom = document.querySelector('.cart')
+const cartOverlay = document.querySelector('.cart-overlay')
+console.log(cartDom);
